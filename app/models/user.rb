7c3dd27cff41bf26b726ec5d0bdd21141fb6767f
Taya Kyouhei <taya_mary919@icloud.com>
2020-07-08 19:16:00 +0900

    新規登録作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum like_team: {
+  	特に無し:0, 巨人:1, DeNA:2, 阪神:3, 広島:4, 中日:5, ヤクルト:6,
+    西武:7, ソフトバンク:8, 楽天:9, ロッテ:10, 日本ハム:11, オリックス:12
+    }
 end

054b5dd5816a6b3f77fa06a615bbeb9b4376e13a
Taya Kyouhei <taya_mary919@icloud.com>
2020-07-10 16:19:57 +0900

    プロフィール編集画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,17 @@ class UsersController < ApplicationController
 	
 	def edit
 	    @user = User.find(params[:id])
-	end    	
+	end
+	
+	def update
+	    @user = User.find(params[:id])
+	    if  @user.update(user_params)
+	    	redirect_to user_path(@user)
+	    else
+	        render :edit
+	    end
+	end        	
+	           	
 	
 private
     def user_params
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1e9bd11522cc24b4d07e8e7539eda1f3ab95997b38623bd767adde89fd35c10954b471e5604406edd3236273303e3436c7545ae12229fddd50b3e48f2b204bd2'

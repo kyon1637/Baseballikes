a56b18edc4c5aba78ee85663fe9a0fdbc2dd77a4
Taya Kyouhei <taya_mary919@icloud.com>
2020-07-15 16:49:59 +0900

    コメント機能の非同期通信実装

--- app/controllers/baseball_comments_controller.rb
+++ app/controllers/baseball_comments_controller.rb
@@ -5,16 +5,15 @@ class BaseballCommentsController < ApplicationController
 		@baseball_comment = @baseball.baseball_comments.new(baseball_comment_params)
         @baseball_comment.user_id = current_user.id
 		@baseball_comment.save
-		redirect_to baseball_path(@baseball)
 	end
 	
 	def destroy
-		@baseball_comment = BaseballComment.find(params[:baseball_id])
+		@baseball_comment = BaseballComment.find(params[:id])
 		@baseball_comment.destroy
 	end	
 
 	private
 	def baseball_comment_params
-		params.require(:baseball_comment).permit(:comment)
+		params.require(:baseball_comment).permit(:comment, :baseball_id, :user_id)
 	end	
 end

1b9de0e78ae51b6b8f9b7cd164f1be8276de9e4c
taya kyohei <taya_mary919@icloud.com>
2020-07-26 12:42:13 +0000

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '<18.190.111.197>', user: 'ec2-user', roles: %w{app web db}
+server '18.190.111.197', user: 'ec2-user', roles: %w{app web db}
